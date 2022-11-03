6f137abe530745605e966d3c01679e752ce9563a
Essteel <siannon.steel@gmail.com>
2022-11-03 14:33:56 +1030

    Added Team Model and Schema

---
+++ models/team.py
@@ -0,0 +1,14 @@
+from init import db, ma
+
+class Team(db.Model):
+    __tablename__ = 'teams'
+
+    id = db.Column(db.Integer, primary_key=True)
+    team_name = db.Column(db.String, nullable=False)
+    total_won = db.Column(db.Integer, default=0)
+    total_drawn = db.Column(db.Integer, default=0)
+    total_lost = db.Column(db.Integer, default=0)
+
+class TeamSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'team_name', 'total_won', 'total_drawn', 'total_lost')
